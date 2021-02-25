da708d3868c632a645314212f11787f26e7b7396
Jordan Neumann <62191356+Jordan-Neumann@users.noreply.github.com>
2021-02-25 16:50:10 -0600

    Updating py file

--- documents/Indeed_Job_Search.py
+++ documents/Indeed_Job_Search.py
@@ -3,7 +3,7 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
+import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -15,14 +15,20 @@ driver = webdriver.Chrome(PATH, options=options)
 
 driver.get("https://www.indeed.com/")
 search = driver.find_element_by_id("text-input-what")
-search.send_keys("data")
+search.send_keys("data science")
 search.send_keys(Keys.ENTER)
-time.sleep(2)
+time.sleep(5)
 
 search = driver.find_element_by_id("where")
 search.clear()
 search.send_keys("United States")
 search.send_keys(Keys.ENTER)
+time.sleep(5)
+
+link = driver.find_element_by_class_name("no-wrap")
+link = link.find_element_by_partial_link_text("date")
+link.send_keys(Keys.ENTER)
+
 
 try:
     resultsCol = WebDriverWait(driver, 10).until(
@@ -41,26 +47,37 @@ except:
     driver.quit()
 
 results = driver.find_element_by_id("resultsCol")
+
 titles = results.find_elements_by_tag_name("h2")
 companies = driver.find_elements_by_class_name("company")
 ratings = driver.find_elements_by_class_name("ratingsContent")
+summary = driver.find_elements_by_class_name("summary")
 
-print(len(companies))
-for t, c, r in zip(titles, companies, ratings):
+joblist = []
+
+for t, c, r, s in zip(titles, companies, ratings, summary):
     title = t.find_element_by_tag_name("a")
     title = title.get_attribute('textContent')
-    title = title.lstrip()
-    title = title.rstrip()
-
-    rating = r.get_attribute("textContent")
-    rating = rating.rstrip()
+    title = title.strip()
 
     company = c.get_attribute('textContent')
-    print(title)
-    print(company)
-    print(rating)
-    print()
-    print()
+    company = company.strip()
+
+    rating = r.get_attribute("textContent")
+    rating = rating.strip()
+
+    summary = s.get_attribute("textContent")
+    summary = summary.strip()
+
+    jobs = {'title': title,
+            'company': company,
+            'rating': rating,
+            'summary': summary}
+    
+    joblist.append(jobs)
+
+df = pd.DataFrame(joblist)
+df.to_csv('jobs.csv', index = False, header = True)
 
 time.sleep(10)
 driver.quit()
